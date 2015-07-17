d14bedde4619e14e5d42f2edba5dd08458eb6963
Jens Gustedt <Jens.Gustedt@inria.fr>
2015-07-17 23:13:11 +0200

    move the atomic_lock features to macros

--- atomic_lock.c
+++ atomic_lock.c
@@ -2,12 +2,9 @@
 #include "atomic_lock.h"
 
 void (atomic_lock_unlock)(volatile atomic_lock* f){
-  atomic_flag_clear_explicit(f, memory_order_release);
+  atomic_lock_unlock(f);
 }
 
 void (atomic_lock_lock)(volatile atomic_lock* f){
-  do {
-    // busy loop
-  } while (atomic_flag_test_and_set_explicit(f, memory_order_consume));
-  atomic_thread_fence(memory_order_acq_rel);
+  atomic_lock_lock(f);
 }
--- atomic_lock.h
+++ atomic_lock.h
@@ -8,4 +8,15 @@ typedef atomic_flag atomic_lock;
 void atomic_lock_lock(volatile atomic_lock*);
 void atomic_lock_unlock(volatile atomic_lock*);
 
+#define atomic_lock_unlock(F) atomic_flag_clear_explicit((F), memory_order_release)
+
+#define atomic_lock_lock(F)                                             \
+({                                                                      \
+  atomic_lock volatile* atomic_lock_lock = (F);                         \
+  do {                                                                  \
+    /* busy loop */                                                     \
+  } while (atomic_flag_test_and_set_explicit(atomic_lock_lock, memory_order_acquire)); \
+ })
+
+
 #endif
