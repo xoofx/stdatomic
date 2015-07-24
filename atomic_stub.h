ec28a82aa5ee7e30f1f0d158d8f3777b9f845b81
Jens Gustedt <Jens.Gustedt@inria.fr>
2015-07-24 13:51:15 +0200

    don't have the replacements think that they have an atomic as second argument
    they are just seen as pointers, if we end up there

--- atomic_stub.h
+++ atomic_stub.h
@@ -2,7 +2,7 @@
 #define _STDATOMIC_STUB_H_ 1
 
 #define INSTANTIATE_STUB_LC(N, T)                                       \
-T __atomic_fetch_add_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
+T __atomic_fetch_add_ ## N ## _internal(void* _X, T const _V, int _mo) { \
   T exp = 0;                                                            \
   T rep = _V;                                                           \
   int mof = _mo == memory_order_relaxed ? memory_order_relaxed : memory_order_consume; \
@@ -11,7 +11,7 @@ T __atomic_fetch_add_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
   }                                                                     \
   return exp;                                                           \
 }                                                                       \
-T __atomic_fetch_sub_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
+T __atomic_fetch_sub_ ## N ## _internal(void* _X, T const _V, int _mo) { \
   T exp = 0;                                                            \
   T rep = _V;                                                           \
   int mof = _mo == memory_order_relaxed ? memory_order_relaxed : memory_order_consume; \
@@ -20,7 +20,7 @@ T __atomic_fetch_sub_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
   }                                                                     \
   return exp;                                                           \
 }                                                                       \
-T __atomic_fetch_and_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
+T __atomic_fetch_and_ ## N ## _internal(void* _X, T const _V, int _mo) { \
   T exp = 0;                                                            \
   T rep = _V;                                                           \
   int mof = _mo == memory_order_relaxed ? memory_order_relaxed : memory_order_consume; \
@@ -29,7 +29,7 @@ T __atomic_fetch_and_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
   }                                                                     \
   return exp;                                                           \
 }                                                                       \
-T __atomic_fetch_xor_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
+T __atomic_fetch_xor_ ## N ## _internal(void* _X, T const _V, int _mo) { \
   T exp = 0;                                                            \
   T rep = _V;                                                           \
   int mof = _mo == memory_order_relaxed ? memory_order_relaxed : memory_order_consume; \
@@ -38,7 +38,7 @@ T __atomic_fetch_xor_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
   }                                                                     \
   return exp;                                                           \
 }                                                                       \
-T __atomic_fetch_or_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
+T __atomic_fetch_or_ ## N ## _internal(void* _X, T const _V, int _mo) { \
   T exp = 0;                                                            \
   T rep = _V;                                                           \
   int mof = _mo == memory_order_relaxed ? memory_order_relaxed : memory_order_consume; \
@@ -47,20 +47,20 @@ T __atomic_fetch_or_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
   }                                                                     \
   return exp;                                                           \
 }                                                                       \
-T __atomic_load_ ## N ## _internal(_Atomic(T)* _X, int _mo) {           \
+T __atomic_load_ ## N ## _internal(void* _X, int _mo) {           \
   T ret;                                                                \
   __atomic_load_internal(N, _X, &ret, _mo);                             \
   return ret;                                                           \
 }                                                                       \
-void __atomic_store_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
+void __atomic_store_ ## N ## _internal(void* _X, T const _V, int _mo) { \
   __atomic_store_internal(N, _X, &_V, _mo);                             \
 }                                                                       \
-T __atomic_exchange_ ## N ## _internal(_Atomic(T)* _X, T const _V, int _mo) { \
+T __atomic_exchange_ ## N ## _internal(void* _X, T const _V, int _mo) { \
   T ret;                                                                \
   __atomic_exchange_internal(N, _X, &_V, &ret, _mo);                    \
   return ret;                                                           \
 }                                                                       \
-_Bool __atomic_compare_exchange_ ## N ## _internal(_Atomic(T)* _X, T* _E, T const _V, int _mos, int _mof) { \
+_Bool __atomic_compare_exchange_ ## N ## _internal(void* _X, T* _E, T const _V, int _mos, int _mof) { \
   return __atomic_compare_exchange_internal(N, _X, _E, &_V, _mos, _mof); \
 }
 
