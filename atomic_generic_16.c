643d23394ac4810efbe8dd56b5108591000c1df7
Jens Gustedt <Jens.Gustedt@inria.fr>
2015-07-26 09:46:58 +0200

    also produce the arithmetic stubs for 128 bit integers, if they exist

--- atomic_generic_16.c
+++ atomic_generic_16.c
@@ -7,5 +7,9 @@ INSTANTIATE_SYNC(16, __impl_uint128_t)
 #else
 INSTANTIATE_STUB_LCM(16, __impl_uint128_t)
 INSTANTIATE_SYNCM(16, __impl_uint128_t)
+# if __UINT128__
+INSTANTIATE_STUB_LCA(16, __impl_uint128_t)
+INSTANTIATE_SYNCA(16, __impl_uint128_t)
+# endif
 #endif
 
